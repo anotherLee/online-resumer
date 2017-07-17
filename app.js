165b964d2b6fdd56724297eda0c237ae1226506f
limingru <18813070947@163.com>
2017-07-17 09:17:24 +0800

    todoList基本做完，但是还有一些功能没加

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 import bar from './bar'
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import './page.scss'
+
+Vue.use(ElementUI)
+
 
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    newTodo:'',
+    todoList:[],
+    a:-1
+  },
+  methods:{
+    addTodo: function(){
+      this.todoList.push({
+        title: this.newTodo,
+        createdAt: new Date(),
+        done: false
+      })
+      this.newTodo = ''
+    },
+    removeTodo: function(index){
+      this.todoList.splice(index,1)
+    }
+
+  },
+  created: function(){
+    window.onbeforeunload = ()=>{
+      let dataString = JSON.stringify(this.todoList)
+      window.localStorage.setItem('myTodos',dataString)
+    }
+    let oldDataString = window.localStorage.getItem('myTodos')
+    let oldData = JSON.parse(oldDataString)
+    this.todoList = oldData || []
+
+
+
   }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015'}
+      {test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015'},
+      {test:/\.css$/,loader:'style-loader!css-loader'},
+      {test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/, loader: 'file-loader'},
+      {test: /\.scss$/, exclude: /node_modules/, loader: 'style-loader!css-loader!sass-loader' }
     ]
   },
   resolve: {
@@ -14,4 +17,21 @@ module.exports = {
       'vue$': 'vue/dist/vue.common.js'
     }
   }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ])
 }
