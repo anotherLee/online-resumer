7a25446ed171f0b94df39ff5937ec53d902e6ff3
limingru <18813070947@163.com>
2017-07-17 22:41:17 +0800

    给todoList添加了登陆功能

--- app.js
+++ app.js
@@ -3,45 +3,92 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import './page.scss'
+import AV from 'leancloud-storage'
 
 Vue.use(ElementUI)
 
+var APP_ID = 'aboJszqbrLt3tvFdbNmqozOx-gzGzoHsz';
+var APP_KEY = 'vD6DjXUrNUflDeABzoR8jS01';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+
+
+
+
+
+
 
 var app = new Vue({
-  el: '#app',
-  data: {
-    newTodo:'',
-    todoList:[],
-    a:-1
-  },
-  methods:{
-    addTodo: function(){
-      this.todoList.push({
-        title: this.newTodo,
-        createdAt: new Date(),
-        done: false
-      })
-      this.newTodo = ''
+    el: '#app',
+    data: {
+        newTodo: '',
+        todoList: [],
+        actionType:'register',
+        formData:{
+            email:'',
+            username:'',
+            password:''
+        },
+        currentUser: null
+
     },
-    removeTodo: function(index){
-      this.todoList.splice(index,1)
+    methods: {
+        addTodo: function () {
+            this.todoList.push({
+                title: this.newTodo,
+                createdAt: new Date(),
+                done: false
+            })
+            this.newTodo = ''
+        },
+        removeTodo: function (index) {
+            this.todoList.splice(index, 1)
+        },
+        register:function(){
+            let user = new AV.User()
+            user.setEmail(this.formData.email)
+            user.setUsername(this.formData.username)
+            user.setPassword(this.formData.password)
+            user.signUp().then((loginedUser)=>{
+                this.currentUser = this.getCurrentUser()
+            }, (error)=>{
+                alert('注册失败')
+            })
+            this.clear()
+        },
+        login: function(){
+            AV.User.logIn(this.formData.email, this.formData.password).then((loginedUser)=>{
+                this.currentUser = this.getCurrentUser()
+            },(error)=>{
+                alert('登陆失败')
+            })
+            this.clear()
+        },
+        getCurrentUser: function(){
+            let id = AV.User.current().id
+            let createdAt = AV.User.current().createdAt
+            let username = AV.User.current().attributes.username
+            return {id,createdAt,username}
+        },
+        clear: function(){
+            this.formData.username = ''
+            this.formData.email = ''
+            this.formData.password = ''
+        }
+
+    },
+    created: function () {
+        window.onbeforeunload = () => {
+            let dataString = JSON.stringify(this.todoList)
+            window.localStorage.setItem('myTodos', dataString)
+        }
+        let oldDataString = window.localStorage.getItem('myTodos')
+        let oldData = JSON.parse(oldDataString)
+        this.todoList = oldData || []
+
+
     }
-
-  },
-  created: function(){
-    window.onbeforeunload = ()=>{
-      let dataString = JSON.stringify(this.todoList)
-      window.localStorage.setItem('myTodos',dataString)
-    }
-    let oldDataString = window.localStorage.getItem('myTodos')
-    let oldData = JSON.parse(oldDataString)
-    this.todoList = oldData || []
-
-
-
-  }
 })
-
-setInterval(function(){
-  console.log(app.a)
-},2000)
